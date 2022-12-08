07b3a50304a5990a754b6fb3e5149ef1e1d7be29
Leon Yang <47435125+lyango@users.noreply.github.com>
2022-12-07 22:25:35 -0800

    Updated job experience

--- assets/js/type.js
+++ assets/js/type.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["a software engineer.", "a game developer.", "a programmer."];
+const textArray = ["a software engineer.", "a game developer.", "a developer.", "a programmer."];
 const typingDelay = 80;
 const erasingDelay = 80;
 const newTextDelay = 2000; // Delay between current and next text
